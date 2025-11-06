da01e999a8445098e870cad1d9ddf8b3b7dfc36b
Yaroslav <trifonovyaroslav@internet.ru>
2025-11-06 11:45:25 +0300

    Minor changes

--- js/script.js
+++ js/script.js
@@ -2,13 +2,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const splash = document.getElementById('splash')
   const enterBtn = document.getElementById('blob-btn')
 
-  enterBtn.addEventListener('click', () => {
-    splash.classList.add('hidden')
-    setTimeout(() => {
+  const KEY = 'splashSeen'
+
+  if (sessionStorage.getItem(KEY) === 'true') {
+    if (splash) {
       splash.style.display = 'none'
-      window.scrollTo({ top: 0, behavior: 'smooth' })
-    }, 600)
-  })
+    }
+    return
+  }
+
+  if (enterBtn) {
+    enterBtn.addEventListener('click', () => {
+      sessionStorage.setItem(KEY, 'true')
+
+      splash.classList.add('hidden')
+      setTimeout(() => {
+        splash.style.display = 'none'
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+      }, 600)
+    })
+  }
 })
 
 document.querySelectorAll('nav a').forEach((anchor) => {
