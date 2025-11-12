5b807359b2daf8c777806288e797a40b308205d5
Yaroslav <trifonovyaroslav@internet.ru>
2025-11-12 11:24:10 +0300

    Add slider

---
+++ js/slider.js
@@ -0,0 +1,30 @@
+const slider = document.querySelector(".slider");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+const slides = Array.from(slider.querySelectorAll("img"));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+prevButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+function updateSlider() {
+  const slideWidth = document.querySelector(".slider-container").clientWidth;
+  slider.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+}
+
+updateSlider();
+
+setInterval(() => {
+  showNextSlide();
+}, 5000);
