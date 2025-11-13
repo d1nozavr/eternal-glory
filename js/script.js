0b63b6b941901c5766ea96b52a081213866aa459
Yaroslav <trifonovyaroslav@internet.ru>
2025-11-13 12:34:37 +0300

    Add all files

---
+++ js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const splash = document.getElementById("splash");
+  const enterBtn = document.getElementById("button-64");
+
+  const KEY = "splashSeen";
+
+  if (sessionStorage.getItem(KEY) === "true") {
+    if (splash) {
+      splash.style.display = "none";
+    }
+    return;
+  }
+
+  if (enterBtn) {
+    enterBtn.addEventListener("click", () => {
+      sessionStorage.setItem(KEY, "true");
+
+      splash.classList.add("hidden");
+      setTimeout(() => {
+        splash.style.display = "none";
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }, 600);
+    });
+  }
+});
